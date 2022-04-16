28cee566e29b999543529d5b7cc67b2c8cf25d2d
Akme <you@example.com>
2022-04-16 01:03:39 -0400

    home_pagev1

--- src/components/HomeComponent/Home.tsx
+++ src/components/HomeComponent/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="container-fluid home-component p-0 m-0 me-0 ms-0 d-flex flex-column">
       <div className="home-btn-txt">
         <p className="text-info fs-3">A place where your love can grow</p>
-        <div className="btn btn-secondary border border-primary shadow-lg text-primary home-btn p-4 fs-2">View love</div>
+        <div className="btn btn-secondary border border-info shadow-lg text-primary home-btn p-4 fs-2">View love</div>
       </div>
     </div>
   )
